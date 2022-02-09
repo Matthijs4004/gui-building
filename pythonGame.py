1d6265601da9071276d2202765f728b8948ec160
Matthijs4004 <matthijs4004@gmail.com>
2022-02-09 10:15:30 +0100

    pythonGame gui update afgemaakt

--- pythonGame.py
+++ pythonGame.py
@@ -1,5 +1,3 @@
-from cgitb import text
-from optparse import Values
 import tkinter as tk
 from tkinter import font, ttk
 from tkinter import messagebox
@@ -16,7 +14,11 @@ bosvliegtuigInput = tk.StringVar()
 grotInput = tk.StringVar()
 slaapplekInput2 = tk.StringVar()
 huisInput = tk.StringVar()
-getallen = list(range(1,100))
+huis2Input = tk.StringVar()
+radioInput = tk.StringVar()
+frequentieInput = tk.IntVar()
+bootInput = tk.StringVar()
+getallen = list(range(1,1000))
 jaNee = ["Ja","Nee"]
 bosVliegtuig = ["Bos", "Vliegtuig"]
 
@@ -25,8 +27,17 @@ Je wordt wakker zonder enig idee waar je bent in het wrak van het neergestortte 
 Je loopt naar de opengebarstte achterkant van het vliegtuig en ziet een bijl op de grond liggen.
 (Onthoud de frequentie 208)"""
 verhaaltje2 = """Vervolgens ga je vanaf het vliegtuig het bos in.
-Je hebt nog nergens om de nacht door te brengen dus,
-"""
+Je hebt nog nergens om de nacht door te brengen dus,"""
+verhaaltje3 = """Je gaat het luik naar beneden en komt terecht in een ruimte waarin allemaal spullen staan,
+zoals een generator, lampen, een betere bijl en het lijkt erop dat er ook een radio staat.
+Je zet de generator aan."""
+verhaaltje4 ="""Het lijkt erop dat de radio het nog doet en je besluit contact te zoeken met iemand via de radio.
+Je moet een de radio instellen op een frequentie(hij is al eens eerder voorgekomen"""
+verhaaltje5 ="""De volgende dag hoor je de boot al in de verte aankomen
+Je stapt op de boot zodra die aanmeert en daarmee hebt je get spel gewonnen
+-------------------------------------
+|             You won!              |
+-------------------------------------"""
 def incorrect(text):
     messagebox.showinfo("Incorrect",text)
 def gameover(text):
@@ -85,7 +96,43 @@ def LevelTwee_6():
     vraag.configure(text="Ga je het huis in?")
     vraag.place(x=250)
     antwoordV.configure(textvariable=huisInput,values=jaNee)
-    button.configure(command=LevelTweeCheck_5)
+    button.configure(command=LevelTweeCheck_6)
+def LevelDrie():
+    level.configure(text="Level 3 - Het huis")
+    verhaal.configure(text="Binnen in het huis ziet het er erg verlaten uit, maar er valt wat op in de grond.\nHet is een luik in de grond van het huis ")
+    verhaal.place(x=110)
+    vraag.configure(text="Ga je naar beneden in het luik?")
+    vraag.place(x=210)
+    antwoordV.configure(textvariable=huis2Input,values=jaNee)
+    button.configure(command=LevelDrieCheck)
+def LevelVier():
+    level.configure(text="Level 4 - de Kelder")
+    verhaal.configure(text=verhaaltje3)
+    verhaal.place(x=50)
+    vraag.configure(text="Ga je de radio gebruiken?")
+    vraag.place(x=230)
+    antwoordV.configure(textvariable=radioInput,values=jaNee)
+    button.configure(command=LevelVierCheck_1)
+def LevelVier_2():
+    verhaal.configure(text=verhaaltje4)
+    verhaal.place(x=50)
+    vraag.configure(text="Welke kies je?")
+    vraag.place(x=260)
+    antwoordV.configure(textvariable=frequentieInput,values=getallen)
+    button.configure(command=LevelVierCheck_2)
+def LevelVier_3():
+    verhaal.configure(text="De man op de radio stelt voor om een boot te varen richting het eiland.")
+    verhaal.place(x=120)
+    vraag.configure(text="Neem je dit offer aan? ")
+    vraag.place(x=240)
+    antwoordV.configure(textvariable=bootInput,values=jaNee)
+    button.configure(command=LevelVierCheck_3)
+def Ending():
+    verhaal.configure(text=verhaaltje5)
+    verhaal.place(x=70)
+    vraag.destroy()
+    antwoordV.destroy()
+    button.configure(command=window.destroy,text="Afsluiten")
 
 def LevelTweeCheck_1():
     antwoord = slaapplekInput.get()
@@ -125,14 +172,41 @@ def LevelTweeCheck_5():
     else:
         gameover("Op de weg terug naar je slaapplek wordt je overvallen door een monster op het eiland.")
 def LevelTweeCheck_6():
-    antwoord = input("> ")
+    antwoord = huisInput.get()
     if antwoord == "Ja":
         volgende()
-        #huis_level()
+        LevelDrie()
     else:
         gameover("Het is nog nacht en dus komt er een monster uit de bosjes tevoorschijn die jouw vermoord.")
-
-
+def LevelDrieCheck():
+    antwoord = huis2Input.get()
+    if antwoord == "Ja":
+        volgende()
+        LevelVier()
+    else:
+        gameover("Het is nog nacht en er komt een monster het huis in die je om het leven brengt.")
+def LevelVierCheck_1():
+    antwoord = radioInput.get()
+    if antwoord.lower().strip() == "ja":
+        volgende()
+        LevelVier_2()
+    else:
+        gameover("Je hebt gekozen de radio niet te gebruiken, maar het is de enige manier van het eiland af.")
+def LevelVierCheck_2():
+    antwoord = frequentieInput.get()
+    if antwoord == 208:
+        volgende()
+        LevelVier_3()
+    else:
+        gameover("Dit is niet de correcte frequentie en daarmee heb je gefaald om iemand te bereiken aan de andere kant van de radio.")
+def LevelVierCheck_3():
+    antwoord = bootInput.get()
+    if antwoord == "Ja":
+        volgende()
+        Ending()
+    else:
+        gameover("Je hebt het offer niet aangenomen en daarmee hebt je de verkeerde keuze gemaakt. De boot is de enige manier van het eiland af.")
+        
 level = tk.Label(text="Level 1 - Vliegtuig",font=("Calibri Light", 11, font.BOLD))
 level.place(y=0,x=255)
 verhaal = tk.Label(text=verhaaltje1, font=("Calibri Light", 11))
